bf1c825dca4f82e0db273cdd04c0977831c9de76
Mikhail Lugovoy <lugovoy.misha@gmail.com>
2016-08-20 19:00:17 +0300

    master

---
+++ src/main/java/com/test/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by milu on 20.08.16.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String home(){
+        return "home";
+    }
+
+}
---
+++ src/main/java/com/test/util/AppConfig.java
@@ -0,0 +1,28 @@
+package com.test.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by milu on 20.08.16.
+ */
+@Configuration
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
