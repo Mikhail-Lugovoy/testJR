c924758c5a38cd427c5af638a4d257f2610d682a
Mikhail Lugovoy <lugovoy.misha@gmail.com>
2016-08-27 15:14:53 +0300

    add name, age, script

--- script.sql
+++ script.sql
@@ -1,13 +1,14 @@
--- CREATE SCHEMA `test` DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA `test` DEFAULT CHARACTER SET utf8;
 
--- CREATE TABLE `test`.`users` (
--- `id` INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- `name` VARCHAR(25) NOT NULL,
--- `age` INT NOT NULL,
--- `isAdmin` BIT(1) NOT NULL DEFAULT false,
--- `createdDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
---
--- INSERT INTO `test`.`users` (`name`, `age`) VALUES ('Kola', '21');
--- INSERT INTO `test`.`users` (`name`, `age`) VALUES ('Vasa', '23');
--- INSERT INTO `test`.`users` (`name`, `age`) VALUES ('Dande', '56');
-INSERT INTO `test`.`users` (`name`, `age`, `isAdmin`) VALUES ('Dande', '56', 1);
+CREATE TABLE `test`.`users` (
+`id` INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(25) NOT NULL,
+`age` INT NOT NULL,
+`isAdmin` BIT(1) NOT NULL DEFAULT false,
+`createdDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+INSERT INTO `test`.`users` (`name`, `age`) VALUES ('Adele', '25');
+INSERT INTO `test`.`users` (`name`, `age`) VALUES ('John', '17');
+INSERT INTO `test`.`users` (`name`, `age`) VALUES ('Rafael', '34');
+INSERT INTO `test`.`users` (`name`, `age`, `isAdmin`) VALUES ('Bruce', '57', 1);
+INSERT INTO `test`.`users` (`name`, `age`, `isAdmin`) VALUES ('Robby', '43', 1);
